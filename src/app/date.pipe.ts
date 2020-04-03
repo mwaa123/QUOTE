ac5c9c7c7862770ae4f220e2ec48491a06f00fc8
ruth <ruthwanjiramugo@gmail.com>
2020-04-03 16:48:41 +0300

    add the return function in the pipe.ts file to return a value

--- src/app/date.pipe.ts
+++ src/app/date.pipe.ts
@@ -11,40 +11,24 @@ export class DatePipe implements PipeTransform {
         if (seconds < 29) // less than 30 seconds ago will show as 'Just now'
             return 'Just now';
         const intervals = {
-//             'year': 31536000,
-//             'month': 2592000,
-//             'week': 604800,
-//             'day': 86400,
-//             'hour': 3600,
-//             'minute': 60,
-//             'second': 1
-//         };
-//         let counter;
-//         for (const i in intervals) {
-//             counter = Math.floor(seconds / intervals[i]);
-//             if (counter > 0)
-//                 if (counter === 1) {
-//                     return counter + ' ' + i + ' ago'; // singular (1 day ago)
-//                 } else {
-//                     return counter + ' ' + i + 's ago'; // plural (2 days ago)
-//                 }
-//         }
-//     }
-//     return value;
-// }}
-//   //   transform(value: any, ...args: any[]): any {
-// //     let date:Date =new Date();
-// //     let updateDate:any= new Date(date.getFullYear(),date.getMonth(),date. getDate());
-// //     let dateDifference=Math.abs(updateDate.value);
-// //     const secondsInDay=86400;
-// //     let dateDifferenceInSeconds=dateDifference*0.001;
-// //     let dateCount=dateDifferenceInSeconds/secondsInDay;
-
-// //     if(dateCount>0.5){
-// //       return Math.ceil(dateCount)+ " "+"days ago";
-// //     }
-// //     else{
-// //     return null;
-// //   }
-// //   }
-// // }
+            'year': 31536000,
+            'month': 2592000,
+            'week': 604800,
+            'day': 86400,
+            'hour': 3600,
+            'minute': 60,
+            'second': 1
+        };
+        let counter;
+        for (const i in intervals) {
+            counter = Math.floor(seconds / intervals[i]);
+            if (counter > 0)
+                if (counter === 1) {
+                    return counter + ' ' + i + ' ago'; // singular (1 day ago)
+                } else {
+                    return counter + ' ' + i + 's ago'; // plural (2 days ago)
+                }
+        }
+    }
+    return value;
+}}
